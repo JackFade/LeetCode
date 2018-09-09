be8146822947d3911fc1621a07842f91f6f42d9e
atom <385805776@qq.com>
2018-09-09 18:07:12 +0800

    L215 done

--- src/main/java/queueAndStack/L215.java
+++ src/main/java/queueAndStack/L215.java
@@ -1,5 +1,9 @@
 package queueAndStack;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /*
 * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 
@@ -16,9 +20,27 @@ package queueAndStack;
 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
 * */
 public class L215 {
-    class Solution {
-        public int findKthLargest(int[] nums, int k) {
-            return 0;
+    public static void main(String[] args) {
+        System.out.println(new Solution().findKthLargest(new int[]{2,1}, 1));
+    }
+}
+
+class Solution {
+
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        for (int i = 0; i < nums.length; i++) {
+            if (queue.size() < k) {
+                queue.add(nums[i]);
+            } else if (queue.peek() < nums[i]){
+                queue.poll();
+                queue.add(nums[i]);
+            }
         }
+        return queue.peek();
     }
 }
