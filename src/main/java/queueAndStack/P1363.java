bfbea0bab5e2c9a4b1d70c5f18353b5a81fa8914
atom <385805776@qq.com>
2018-09-09 16:27:32 +0800

    P1363 done

--- src/main/java/queueAndStack/P1363.java
+++ src/main/java/queueAndStack/P1363.java
@@ -1,4 +1,34 @@
 package queueAndStack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class P1363 {
+    public static void main(String[] args) {
+        P1363 runner = new P1363();
+        System.out.println(runner.checkVaild(new int[]{3,1,2,5,4}));
+    }
+
+    boolean checkVaild(int[] arr) {
+        Stack<Integer> stack = new Stack<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            queue.add(arr[i]);
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            stack.push(i);
+            while (!queue.isEmpty() && queue.peek() == stack.peek()) {
+                queue.poll();
+                stack.pop();
+            }
+        }
+
+        if (!stack.empty()) {
+            return false;
+        }
+        return true;
+    }
 }
