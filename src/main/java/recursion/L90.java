5c977e679dadc5c32e9e9c1fca8a903dc0ed53f5
atom <385805776@qq.com>
2018-10-28 23:39:15 +0800

    init recursion

---
+++ src/main/java/recursion/L90.java
@@ -0,0 +1,33 @@
+package recursion;
+
+import java.util.List;
+
+
+/*
+给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+
+说明：解集不能包含重复的子集。
+
+示例:
+
+输入: [1,2,2]
+输出:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+
+*/
+
+public class L90 {
+}
+
+class Solution90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        return null;
+    }
+}
