a0fc09a1b5df4012d6f8be0ca475e6fdd7cabc95
atom <385805776@qq.com>
2018-10-28 20:24:32 +0800

    L452 Done

--- src/main/java/greedy/L452.java
+++ src/main/java/greedy/L452.java
@@ -1,6 +1,9 @@
 package greedy;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /*
 在二维空间中有许多球形的气球。对于每个气球，提供的输入是水平方向上，气球直径的开始和结束坐标。由于它是水平的，所以y坐标并不重要，因此只要知道开始和结束的x坐标就足够了。开始坐标总是小于结束坐标。平面内最多存在104个气球。
 
@@ -18,10 +21,33 @@ Example:
 对于该样例，我们可以在x = 6（射爆[2,8],[1,6]两个气球）和 x = 11（射爆另外两个气球）。
 * */
 public class L452 {
+    public static void main(String[] args) {
+        System.out.println(new Solution452().findMinArrowShots(new int[][]{new int[]{10,16},new int[]{2,8},new int[]{1,6},new int[]{7,12}}));
+    }
 }
 
 class Solution452 {
     public int findMinArrowShots(int[][] points) {
-        return 0;
+        if (points.length == 1) {
+            return 1;
+        }
+
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return Integer.compare(o1[0],o2[0]);
+            }
+        });
+
+        int arrowCnt = 0;
+        int maxEnd = -1;
+        for (int i = 0; i < points.length; i++) {
+            if (points[i][0] > maxEnd) {
+                arrowCnt++;
+                maxEnd = points[i][1];
+            }
+            maxEnd = Math.min(points[i][1], maxEnd);
+        }
+        return arrowCnt;
     }
 }
