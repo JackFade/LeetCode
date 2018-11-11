3d8527831e75f658c25eb1a23f4d6f2710680a42
atom <385805776@qq.com>
2018-11-11 19:32:36 +0800

    L90 done

--- src/main/java/recursion/L90.java
+++ src/main/java/recursion/L90.java
@@ -1,6 +1,6 @@
 package recursion;
 
-import java.util.List;
+import java.util.*;
 
 
 /*
@@ -24,10 +24,60 @@ import java.util.List;
 */
 
 public class L90 {
+    public static void main(String[] args) {
+        System.out.println(new Solution90().subsetsWithDup2(new int[]{1,2,2,3,3}));
+    }
 }
 
 class Solution90 {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        return null;
+
+        Set<List<Integer>> result = new HashSet<>();
+        result.add(new ArrayList<>());
+        Arrays.sort(nums);
+
+        generateResult(0, nums, new ArrayList<Integer>(), result);
+
+        List<List<Integer>> resultList = new ArrayList<>(result.size());
+        for (List<Integer> integers : result) {
+            resultList.add(integers);
+        }
+        return resultList;
+    }
+
+    private void generateResult(int i, int[] nums, ArrayList<Integer> item, Set<List<Integer>> result) {
+        if (i >= nums.length) {
+            return;
+        }
+
+        generateResult(i+1, nums, item, result);
+        item.add(nums[i]);
+        result.add(new ArrayList<>(item));
+        generateResult(i+1,nums, item, result);
+        item.remove(item.size() - 1);
+    }
+
+
+
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
+        List<List<Integer>> reslut = new ArrayList<>();
+        Arrays.sort(nums);
+        func(reslut, nums, new ArrayList<>(), 0);
+        return reslut;
+    }
+
+    void func(List<List<Integer>> results, int[] nums, List<Integer> item, int index) {
+
+        results.add(new ArrayList<>(item));
+
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            item.add(nums[i]);
+            func(results, nums, item, i + 1);
+            item.remove(item.size() - 1);
+        }
     }
 }
