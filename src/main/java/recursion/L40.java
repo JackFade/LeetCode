62bd3385a034f2183769e23556f85e5215532d20
atom <385805776@qq.com>
2018-11-11 23:37:55 +0800

    L40 done

--- src/main/java/recursion/L40.java
+++ src/main/java/recursion/L40.java
@@ -1,5 +1,8 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -35,10 +38,40 @@ candidates 中的每个数字在每个组合中只能使用一次。
 */
 
 public class L40 {
+    public static void main(String[] args) {
+        System.out.println(new Solution40().combinationSum2(new int[]{10,1,2,7,6,1,5}, 8));
+    }
 }
 
 class Solution40 {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+
+        generateResult(0, candidates, new ArrayList<>(),0, target, result);
+        return result;
+    }
+
+    private void generateResult(int index, int[] candidates, ArrayList<Integer> items, int itemsSum, int target, List<List<Integer>> result) {
+        for (int i = index; i < candidates.length; i++) {
+            if ((i > index && candidates[i] == candidates[i-1])) {
+                continue;
+            }
+
+            int addItemsSum = itemsSum + candidates[i];
+
+            if (addItemsSum > target) {
+                break;
+            } else if (addItemsSum == target) {
+                List<Integer> targetItem = new ArrayList<>(items);
+                targetItem.add(candidates[i]);
+                result.add(targetItem);
+                break;
+            } else {
+                items.add(candidates[i]);
+                generateResult(i + 1, candidates, items, addItemsSum, target, result);
+                items.remove(items.size() - 1);
+            }
+        }
     }
 }
