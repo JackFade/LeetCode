96bc65d324076dec5978fedd86f7f864bf966432
atom <385805776@qq.com>
2018-11-17 16:57:20 +0800

    L51 done

--- src/main/java/recursion/L51.java
+++ src/main/java/recursion/L51.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -34,10 +36,55 @@ n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并
 */
 
 public class L51 {
+    public static void main(String[] args) {
+        System.out.println(new Solution51().solveNQueens(4));
+    }
 }
 
 class Solution51 {
     public List<List<String>> solveNQueens(int n) {
-        return null;
+        List<List<String>> result = new ArrayList<>();
+
+        solveNQueens(0, n, new ArrayList<Integer>(0), result);
+        return result;
+    }
+
+    private void solveNQueens(int index, int n, List<Integer> queens, List<List<String>> result) {
+        if (index == n) {
+            List<String> item = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                StringBuilder sb = new StringBuilder(n);
+                for (int j = 0; j < n; j++) {
+                    if (j == queens.get(i)) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                item.add(sb.toString());
+            }
+            result.add(item);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (checkSave(i, index, queens)) {
+                List<Integer> newQueens = new ArrayList<>(queens);
+                newQueens.add(i);
+                solveNQueens(index + 1, n, newQueens, result);
+            }
+        }
+    }
+
+    private boolean checkSave(int x, int y, List<Integer> queens) {
+        for (int i = 0; i < queens.size(); i++) {
+            int qy = i;
+            int qx = queens.get(qy);
+            if(qx == x || Math.abs(y-qy) == Math.abs(x - qx)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
