929276098c1559e083a322f8a9303ff704f4ee99
atom <385805776@qq.com>
2018-11-24 10:11:48 +0800

    L34 done

--- src/main/java/divideAndConquer/L34.java
+++ src/main/java/divideAndConquer/L34.java
@@ -1,5 +1,7 @@
 package divideAndConquer;
 
+import java.util.Arrays;
+
 /*
 *
 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
@@ -20,13 +22,51 @@ package divideAndConquer;
 * */
 public class L34 {
     public static void main(String[] args) {
-        System.out.println(new L34.Solution().searchRange(new int[]{5,7,7,8,8,10},8));
+        System.out.println(Arrays.toString(new Solution().searchRange(new int[]{1},8)));
     }
 
-    static class Solution {
-        public int searchRange(int[] nums, int target) {
-            return 0;
+}
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1,-1};
+
+        int start = 0, end = nums.length-1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid;
+                result[1] = mid;
+                int leftStart = start ,leftEnd = mid - 1;
+                while (leftStart <= leftEnd) {
+                    int leftMid = (leftStart + leftEnd) / 2;
+                    if (nums[leftMid] < target) {
+                        leftStart = leftMid + 1;
+                    } else {
+                        result[0] = leftMid;
+                        leftEnd = leftMid - 1;
+                    }
+                }
+
+
+                int rightStart = mid + 1 ,rightEnd = end;
+                while (rightStart <= rightEnd) {
+                    int rightMid = (rightStart + rightEnd) / 2;
+                    if (nums[rightMid] > target) {
+                        rightEnd = rightMid - 1;
+                    } else {
+                        result[1] = rightMid;
+                        rightStart = rightMid + 1;
+                    }
+                }
+                return result;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
+        return result;
     }
 }
 
