3e22c7930c7df52746bf3b09cfbbfa18c7abc540
atom <385805776@qq.com>
2018-11-25 11:09:28 +0800

    L35 Done

--- src/main/java/divideAndConquer/L35.java
+++ src/main/java/divideAndConquer/L35.java
@@ -27,11 +27,30 @@ package divideAndConquer;
 public class L35 {
     public static void main(String[] args) {
         System.out.println(new L35.Solution().searchInsert(new int[]{1,3,5,6},5));
+        System.out.println(new L35.Solution().searchInsert(new int[]{1,3,5,6},2));
+        System.out.println(new L35.Solution().searchInsert(new int[]{1,3,5,6},7));
+        System.out.println(new L35.Solution().searchInsert(new int[]{1,3,5,6},0));
     }
 
     static class Solution {
         public int searchInsert(int[] nums, int target) {
-            return 0;
+            if (nums.length == 0) {
+                return 0;
+            }
+
+            int start = 0, end = nums.length - 1, mid = 0;
+
+            while (start <= end) {
+                mid = (end + start) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            return nums[mid] > target ? mid : mid + 1;
         }
     }
 }
